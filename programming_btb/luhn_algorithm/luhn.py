286189f872fe94069c2dd8fa4fc094cb9a64493c
raysomejuice <123418723+raysomejuice@users.noreply.github.com>
2026-09-13 20:16:53 -0400

    Exercise complete

--- programming_btb/luhn_algorithm/luhn.py
+++ programming_btb/luhn_algorithm/luhn.py
@@ -1,14 +1,14 @@
-import string
-
-def verify(digits:str) -> bool:
+def verify(digits: str) -> bool:
     '''Verify via the Luhn algorithm that a string of decimal digits are valid.
+
+    The dictionary luhn_map maps the input string digit with its digital root.
+    The digital root is calculated for every second digit from the string.
     
     :param digits: str - The string of decimal digits that need to be verified.
     :return: bool - Return true if the string of decimal digits is valid.
     '''
-    luhn_total = 0
-    for index, digit in enumerate(reversed(digits)):
-        digital_root = (1 + index % 2) * int(digit)
-        luhn_total += digital_root // 10 + digital_root
-
-    return luhn_total % 10 == 0 
+    luhn_map = {'0' : 0, '1' : 2, '2' : 4, '3' : 6, '4' : 8, 
+                '5' : 1, '6' : 3, '7' : 5, '8' : 7, '9' : 9}
+    
+    return sum(luhn_map[digit] if index % 2 == 1 else int(digit) 
+           for index, digit in enumerate(reversed(digits))) % 10 == 0
