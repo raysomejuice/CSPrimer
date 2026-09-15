5b1f04d0f75572dcebce1faf107997489abac55c
raysomejuice <123418723+raysomejuice@users.noreply.github.com>
2026-09-14 20:47:43 -0400

    working on make_move function in tic tac toe

--- programming_btb/tic_tac_toe/tic_tac_toe.py
+++ programming_btb/tic_tac_toe/tic_tac_toe.py
@@ -6,10 +6,13 @@ BOARD_POSITIONS = [['', '', ''],
 def draw_board(board: list[list[str]]) -> None:
     pass
 
-def make_move(piece: str, board: list[list[str]]) -> None:
-    pass
+def make_move(piece: str, location: int, board: list[list[str]]) -> None:
+    row = location // 3
+    col = location % 3
+    if not board[row][col]:
+        board[row][col] = piece
 
-def find_winner(piece: str, board: list[list[str]]) -> str:
+def find_winner(piece: str, location: int, board: list[list[str]]) -> str:
     # Determine if a winning move occurred in a diagonal
     if board[0][0] == board[1][1] == board[2][2] == piece or \
        board[0][2] == board[1][1] == board[2][0] == piece or \
@@ -20,5 +23,6 @@ def find_winner(piece: str, board: list[list[str]]) -> str:
        board[0][1] == board[1][1] == board[2][1] == piece or \
        board[0][2] == board[1][2] == board[2][2] == piece:
         return f"Player {piece} is the winner"
-    elif all(not element for row in board for col in row):
-        return
+    if all(not element for row in board for element in row):
+        return "The game is a tie"
+    return "Next move"
