2f93ac4980eb1d8234141fa9aa27d86b935422fb
raysomejuice <123418723+raysomejuice@users.noreply.github.com>
2026-09-16 16:28:19 -0400

    Working on diagonal solutions in winner function

--- programming_btb/tic_tac_toe/tic_tac_toe.py
+++ programming_btb/tic_tac_toe/tic_tac_toe.py
@@ -13,15 +13,29 @@ def make_move(piece: str, location: int, board: list[list[str]]) -> None:
         board[row][col] = piece
 
 def find_winner(piece: str, location: int, board: list[list[str]]) -> str:
-    # Determine if a winning move occurred in a diagonal
-    if board[0][0] == board[1][1] == board[2][2] == piece or \
-       board[0][2] == board[1][1] == board[2][0] == piece or \
-       board[0][0] == board[0][1] == board[0][2] == piece or \
-       board[1][0] == board[1][1] == board[1][2] == piece or \
-       board[2][0] == board[2][1] == board[2][2] == piece or \
-       board[0][0] == board[1][0] == board[2][0] == piece or \
-       board[0][1] == board[1][1] == board[2][1] == piece or \
-       board[0][2] == board[1][2] == board[2][2] == piece:
+    row = location // 3
+    prev_row = (row - 1) % 3
+    next_row = (row1 + 1) % 3
+    col = location % 3
+    prev_col = (col - 1) % 3
+    next_col = (col + 1) % 3
+    diagonal_desc = set(0, 4, 8)
+    diagonal_asc = set(2, 4, 6)
+
+    if board[row][prev_col] == board[row][col] == board[row][next_col] == piece or \
+        board[prev_row][col] == board[row][col] == board[next_row][col] == piece or \
+
+    if locationboard[prev_row][prev_col] == board[row][col] == board[next_row][next_col] == piece or \
+           board[prev_row][next_col] == board[row][col] == board[next_row][prev_col] == piece or \
+
+    # if board[0][0] == board[1][1] == board[2][2] == piece or \
+    #    board[0][2] == board[1][1] == board[2][0] == piece or \
+    #    board[0][0] == board[0][1] == board[0][2] == piece or \
+    #    board[1][0] == board[1][1] == board[1][2] == piece or \
+    #    board[2][0] == board[2][1] == board[2][2] == piece or \
+    #    board[0][0] == board[1][0] == board[2][0] == piece or \
+    #    board[0][1] == board[1][1] == board[2][1] == piece or \
+    #    board[0][2] == board[1][2] == board[2][2] == piece:
         return f"Player {piece} is the winner"
     if all(not element for row in board for element in row):
         return "The game is a tie"
